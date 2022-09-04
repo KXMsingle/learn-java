1d41750e95533333406d14ac8fa9d0e01c0b1074
KXMsingle <112794335+KXMsingle@users.noreply.github.com>
2022-09-04 21:58:45 +0800

    Update JinZhi.java

--- java-03/src/dsad/JinZhi.java
+++ java-03/src/dsad/JinZhi.java
@@ -2,6 +2,6 @@ package dsad;
 
 public class JinZhi {
     public static void main(String[] args) {
-        System.out.println("0b1011");//十六进制
+        System.out.println("0b1011");//二进制
     }
 }
