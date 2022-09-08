578cc3617dc2b600c553f8cfe69ba189a8debf72
KXMsingle <2540562993@qq.com>
2022-09-08 09:52:11 +0800

    总结2

---
+++ 课程2总结/src/运算/YunSuan.java
@@ -0,0 +1,11 @@
+package 运算;
+
+public class YunSuan {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        b=++a;
+        System.out.println("b:"+b);
+        System.out.println("a:"+a);
+    }
+}
